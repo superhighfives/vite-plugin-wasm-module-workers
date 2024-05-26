146fc1127d3fd1d7c72d9beb9daca0a4ef965876
Charlie Gleason <hi@charliegleason.com>
2024-05-26 09:54:54 +0100

    Added plugin

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+// @ts-check
+import * as path from 'path'
+import * as fs from 'fs'
+
+import { ResolvedConfig } from 'vite'
+type LoadResult = string | null
+
+export default function wasmEdgeModule() {
+  const postfix = '.wasm?module'
+  let isDev = false
+
+  return {
+    name: 'vite:wasm-helper',
+    configResolved(config: ResolvedConfig) {
+      isDev = config.command === 'serve'
+    },
+    config() {
+      return { build: { rollupOptions: { external: /.+\.wasm?url$/i } } }
+    },
+    renderChunk(code: string) {
+      if (isDev) return
+      if (!/.*_WASM.*/g.test(code)) return
+
+      let final = code.replaceAll(/(const\s+(\w+))(.*_WASM.*)/g, (s) => {
+        return s.replace(
+          /const\s+(\w+)_(WASM)\s*=\s*"(.*)"/,
+          'import $1_WASM from ".$3"'
+        )
+      })
+
+      final = final.replaceAll(/const { default:(\n|.)*?(;)/gm, (s) => {
+        return s.replace(
+          /const\s{\sdefault:\s(\w+) } = await import\(\n\s+\/\*\s@vite-ignore\s\*\/\n\s+`\${(\w+)}(\n|.)*?(;)/,
+          'const $1 = $2'
+        )
+      })
+
+      return { code: final }
+    },
+    load(id: string): LoadResult {
+      if (!id.endsWith(postfix)) {
+        return null
+      }
+
+      const filePath = id.slice(0, -1 * '?module'.length)
+
+      if (isDev) {
+        return `
+            import fs from "fs"
+    
+            const wasmModule = new WebAssembly.Module(fs.readFileSync("${filePath}"));
+            export default wasmModule;
+            `
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const assetId = (this as any).emitFile({
+        type: 'asset',
+        name: path.basename(filePath),
+        source: fs.readFileSync(filePath),
+      })
+
+      return `
+          import init from "__WASM_ASSET__${assetId}.wasm"
+          export default init
+          `
+    },
+  }
+}
